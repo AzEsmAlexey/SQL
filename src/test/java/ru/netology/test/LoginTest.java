70fd10ec30ba4d179b184ccf4b22fbae229aa2a0
Neto.Changes <infero-sky@yandex.ru>
2023-11-27 11:10:50 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test/LoginTest.java
@@ -0,0 +1,52 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+import static com.codeborne.selenide.Selenide.*;
+
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class LoginTest {
+
+    @AfterAll
+    static void clean() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login with valid login and password")
+    void shouldSuccessfullyLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user is not exist")
+    void shouldGerErrorNotificationIfUserNotExist() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisibility();
+    }
+
+    @Test
+    @DisplayName("Should get error notification is user is exist but used an invalid verification code")
+    void shouldGetErrorNotificationWithValidLoginAndPasswordButInvalidCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisibility();
+
+    }
+}
